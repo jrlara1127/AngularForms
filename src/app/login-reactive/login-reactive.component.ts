cd53e45e7c46450dab90f3e1328b52c6c4b7fbe6
Rodriguez Lara <jrodriguezlara@deloitte.com>
2024-01-08 16:15:12 -0600

    Uploading form project

---
+++ src/app/login-reactive/login-reactive.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { NonNullableFormBuilder, Validators } from '@angular/forms';
+import { createPasswordStrengthValidator } from '../validators/password-strength.validator';
+
+
+@Component({
+  selector: 'login',
+  templateUrl: './login-reactive.component.html',
+  styleUrls: ['./login-reactive.component.css']
+})
+export class LoginReactiveComponent implements OnInit {
+
+  form = this.fb.group({
+    email: ["", {
+      validators: [Validators.required, Validators.email],
+      updateOn: 'blur'}],
+    password: ['', [Validators.required, Validators.minLength(8),
+                      createPasswordStrengthValidator()]]
+          });
+
+  constructor(private fb: NonNullableFormBuilder) {
+
+
+  }
+
+  ngOnInit() {
+
+  }
+
+  login() { 
+  }
+
+  get email() {
+      return this.form.controls['email'];
+  }
+
+  get password() {
+      return this.form.controls['password'];
+  }
+
+  
+  reset() {
+    this.form.reset();
+
+    console.log(this.form.value);
+
+  }
+
+}
